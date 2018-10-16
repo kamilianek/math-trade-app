02ab69d475bc6673cff02bc07299ffe145798318
Kamil Jurczyk <kamisjur@gmail.com>
2018-10-16 23:14:12 +0200

    Added router and redux configuration

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import './App.css';
+import store from './store';
+import AppRouter from './AppRouter';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.store = store.store;
+    this.persistor = store.persistor;
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Provider
+        store={this.store}
+        persistor={this.persistor}
+      >
+        <PersistGate loading={null} persistor={this.persistor}>
+          <AppRouter />
+        </PersistGate>
+      </Provider>
     );
   }
 }
---
+++ src/views/MainPanelView/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class MainPanelView extends React.Component {
+  render() {
+    return (
+      <h1>Main Panel</h1>
+    );
+  }
+}
+
+export default MainPanelView;
