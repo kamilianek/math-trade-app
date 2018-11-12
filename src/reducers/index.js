23ad3e5ab454e04d5a719a423c0fb659b90351b6
Kamil Jurczyk <kamisjur@gmail.com>
2018-11-12 15:00:37 +0100

    export all reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,11 @@
 import authReducer from './auth';
-
+import editionsReducer from './editions';
+import myAssignedProductsReducer from './myAssignedProducts';
+import otherAssignedProductsReducer from './otherAssignedProducts';
 
 export default {
   auth: authReducer,
+  editions: editionsReducer,
+  myAssignedProducts: myAssignedProductsReducer,
+  otherAssignedProducts: otherAssignedProductsReducer,
 };
