b56ae1724f60fa2c50fd47747741ca2b07847e10
Kamil Jurczyk <kamisjur@gmail.com>
2018-12-05 22:55:26 +0100

    added api call to auth action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -4,6 +4,7 @@ import {
   REGISTER_WITH_PASSWORD,
 } from '../reducers/auth';
 
+import authApi from '../api/auth';
 
 export function signOut() {
   return {
@@ -12,44 +13,53 @@ export function signOut() {
 }
 
 export function loginWithPassword(username, password) {
-  return async (dispatch) => {
-    // TODO: api login call
-    // const response = await api.auth.loginEmail(email, password);
-    dispatch({
-      type: LOGIN_FINISHED,
-      token: '1i236jghj12j31y32i1231ku2t',
-      roles: ['ROLE_ADMIN', 'ROLE_USER', 'ROLE_MODERATOR'],
-      loginData: {
-        username,
-      },
-    });
-    // TODO: fetching all needed
-    // return dispatch(settings.fetchSettings());
+  return (dispatch, getState) => {
+    const { apiUrl } = getState().auth;
+    return authApi.standardLogin(apiUrl, {
+      usernameOrEmail: username,
+      password,
+    })
+      .then((response) => {
+        console.log(response);
+        dispatch({
+          type: LOGIN_FINISHED,
+          token: response.accessToken,
+          roles: response.roles,
+          loginData: {
+            username,
+          },
+          userExists: true, // TODO: change on response
+        });
+      }, (error) => {
+        throw error;
+      });
   };
 }
 
+// TODO
 export function changePassword(newPassword) {
-  return async () => {
-    // TODO: api call
-    // const response = await api.auth.(emhloginEmailangePassword(newPassword);
-    const response = `chngedTo:${newPassword}`;
-    return response;
-  };
+  return async () => `changedTo:${newPassword}`;
 }
 
 export function registerWithPassword(data) {
-  return async (dispatch) => {
-    // TODO: api login call
-    console.log('data', data);
-    // const response = await api.auth.registerWithPassword(data);
-    dispatch({
-      type: REGISTER_WITH_PASSWORD,
-      token: '1i236jghj12j31y32i1231ku2t',
-      roles: ['ROLE_USER'],
-      loginData: {
-        username: data.username,
-      },
-    });
+  return (dispatch, getState) => {
+    const { apiUrl } = getState().auth;
+    console.log('state: ', apiUrl);
+    return authApi.standardRegistration(apiUrl, data)
+      .then((response) => {
+        console.log('data: ', response);
+        dispatch({
+          type: REGISTER_WITH_PASSWORD,
+          token: response.accessToken,
+          roles: response.roles,
+          loginData: {
+            username: data.username,
+          },
+          userExists: true, // TODO: change on response
+        });
+      }, (error) => {
+        throw error;
+      });
   };
 }
 
